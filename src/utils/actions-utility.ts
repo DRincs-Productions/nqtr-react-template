01f300ec51e465668716337ebe8446de21799e8e
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-15 21:43:27 +0100

    Refactor gameEnd function to use StepLabelPropsType for improved type consistency

--- src/utils/actions-utility.ts
+++ src/utils/actions-utility.ts
@@ -1,12 +1,11 @@
-import { clearAllGameDatas, getFlag, narration } from "@drincs/pixi-vn";
-import StepLabelProps from "@drincs/pixi-vn/dist/override/StepLabelProps";
+import { clearAllGameDatas, getFlag, narration, StepLabelPropsType } from "@drincs/pixi-vn";
 import { startLabel } from "../labels/startLabel";
 
 export async function goBack(navigate: (path: string) => void) {
     await narration.goBack(navigate)
 }
 
-export function gameEnd(props: StepLabelProps) {
+export function gameEnd(props: StepLabelPropsType) {
     let isTheEnd = getFlag("is_the_end")
     if (isTheEnd) {
         clearAllGameDatas()
