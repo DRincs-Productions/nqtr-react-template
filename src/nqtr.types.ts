662266a74f240188d1780ab417d4df88ddf3c218
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-17 19:48:01 +0100

    Enhance OnRenderGraphicItemProps interface and implement useQueryQuickRooms hook for improved component management

--- src/nqtr.types.ts
+++ src/nqtr.types.ts
@@ -1,9 +1,15 @@
+import { CanvasBaseItem } from "@drincs/pixi-vn";
 import { StepLabelProps } from "@drincs/pixi-vn/dist/override";
+import { ReactElement } from "react";
 import { ImageTimeSlots } from "./model/TimeSlots";
 
 declare module '@drincs/nqtr/dist/override' {
     interface OnRunProps extends StepLabelProps { }
-    interface OnRenderGraphicItemProps extends StepLabelProps { }
+    interface OnRenderGraphicItemProps extends StepLabelProps {
+        addReactComponent: (component: ReactElement) => void;
+        addCanvasComponent: (component: CanvasBaseItem<any>) => void;
+        addImageTimeSlots: (image: ImageTimeSlots) => void;
+    }
     interface GraphicItemInterface extends ImageTimeSlots { }
     interface OnStartEndStageQuest extends StepLabelProps { }
 }
