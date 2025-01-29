c92483757df921032423755dd7fc94ce6902e3ed
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-29 18:35:01 +0100

    Refactor useQueryQuickRooms to utilize navigator for current location; remove unused imports and simplify icon retrieval logic.

--- src/use_query/useQueryNQTRComponents.ts
+++ src/use_query/useQueryNQTRComponents.ts
@@ -1,69 +1,13 @@
-import { RoomInterface } from "@drincs/nqtr";
+import { navigator } from "@drincs/nqtr";
 import { useQuery } from "@tanstack/react-query";
-import { useSnackbar } from "notistack";
-import { ReactElement } from "react";
-import { useTranslation } from "react-i18next";
-import { useMyNavigate } from "../utils/navigate-utility";
 import { INTERFACE_DATA_USE_QUEY_KEY } from "./useQueryInterface";
-
-type IconType = {
-    icon:
-        | {
-              src: string;
-              type: "image";
-          }
-        | {
-              src: ReactElement;
-              type: "react";
-          };
-    disabled: boolean;
-    selected: boolean;
-    room: RoomInterface;
-};
 
 const QUICK_ROOMS_USE_QUEY_KEY = "quick_rooms_use_quey_key";
 export function useQueryQuickRooms() {
-    const navigate = useMyNavigate();
-    const { t } = useTranslation(["ui"]);
-    const { enqueueSnackbar } = useSnackbar();
     return useQuery({
         queryKey: [INTERFACE_DATA_USE_QUEY_KEY, QUICK_ROOMS_USE_QUEY_KEY],
         queryFn: () => {
-            let icons: IconType[] = [];
-            let currentRoom = getCurrentRoom();
-            getCurrenrLocation()
-                ?.getRooms()
-                .forEach((room) => {
-                    let renderImage = room.renderIcon || room.renderImage;
-                    let disabled = room.disabled;
-                    let selected = room.id === currentRoom?.id;
-                    if (!renderImage) {
-                        return;
-                    }
-                    renderImage({
-                        navigate: navigate,
-                        t: t,
-                        notify: (message, variant) => enqueueSnackbar(message, { variant }),
-                        addReactComponent: (conponent) =>
-                            icons.push({
-                                icon: { src: conponent, type: "react" },
-                                disabled,
-                                selected,
-                                room: room,
-                            }),
-                        addImageData: (image) =>
-                            icons.push({
-                                icon: {
-                                    src: typeof image === "string" ? image : image.src,
-                                    type: "image",
-                                },
-                                disabled,
-                                selected,
-                                room: room,
-                            }),
-                    });
-                });
-            return icons;
+            return navigator.currentLocation?.rooms;
         },
     });
 }
