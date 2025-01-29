eb171553e9afc0531af6886b707a3c93aaf790f8
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-29 19:02:09 +0100

    Refactor startLabel to use navigator for current room and storage for flag management; improve code clarity and organization.

--- src/labels/startLabel.ts
+++ src/labels/startLabel.ts
@@ -1,14 +1,14 @@
-import { setCurrentRoom, timeTracker } from "@drincs/nqtr";
-import { newLabel, setFlag } from "@drincs/pixi-vn";
+import { navigator, timeTracker } from "@drincs/nqtr";
+import { newLabel, storage } from "@drincs/pixi-vn";
 import { NAVIGATION_ROUTE } from "../constans";
 import { aliceQuest } from "../quests/aliceQuest";
 import { mcRoom } from "../values/rooms";
 
 const startLabel = newLabel("start", [
     (props) => {
-        setCurrentRoom(mcRoom);
-        setFlag("weekend", timeTracker.isWeekend);
-        setFlag("not_weekend", !timeTracker.isWeekend);
+        navigator.currentRoom = mcRoom;
+        storage.setFlag("weekend", timeTracker.isWeekend);
+        storage.setFlag("not_weekend", !timeTracker.isWeekend);
         aliceQuest.start(props);
         props.navigate(NAVIGATION_ROUTE);
     },
