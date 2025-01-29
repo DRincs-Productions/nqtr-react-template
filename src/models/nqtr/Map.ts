f535ef86495f61248293dd9790110eea3f07fb0c
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-29 07:54:05 +0100

    Refactor Map class to extend MapStoredClass and implement MyMapInterface with enhanced constructor for activities, name, and image properties

--- src/models/nqtr/Map.ts
+++ src/models/nqtr/Map.ts
@@ -1,3 +1,21 @@
+import { MapStoredClass } from "@drincs/nqtr";
+import ActivityBaseInterface from "@drincs/nqtr/dist/interface/ActivityInterface";
 import { MyMapInterface } from "../../nqtr";
+import ImageTimeSlots from "../ImageTimeSlots";
 
-export default class Map implements MyMapInterface {}
+export default class Map extends MapStoredClass implements MyMapInterface {
+    constructor(
+        id: string,
+        props: {
+            activities?: ActivityBaseInterface[];
+            name: string;
+            image: ImageTimeSlots;
+        }
+    ) {
+        super(id, props.activities);
+        this.name = props.name;
+        this.image = props.image;
+    }
+    name: string;
+    image: ImageTimeSlots;
+}
