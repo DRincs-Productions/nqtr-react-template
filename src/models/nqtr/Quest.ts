07fa0b8febdf5bc410853f1a973c8c9caef8aa34
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-29 09:00:33 +0100

    Refactor Quest class to extend QuestStoredClass and implement QuestInterface; enhance constructor and properties for improved functionality and type safety

--- src/models/nqtr/Quest.ts
+++ src/models/nqtr/Quest.ts
@@ -1,3 +1,30 @@
-import { QuestInterface } from "@drincs/nqtr";
+import { QuestInterface, StageInterface } from "@drincs/nqtr";
+import QuestStoredClass from "@drincs/nqtr/dist/classes/quest/QuestStoredClass";
 
-export default class Quest implements QuestInterface {}
+export default class Quest extends QuestStoredClass implements QuestInterface {
+    constructor(
+        id: string,
+        _stages: StageInterface[],
+        props?: {
+            onStart?: (stage: QuestInterface, props: OnStartEndStageQuest) => void;
+            onNextStage?: (stage: QuestInterface, props: OnStartEndStageQuest) => void;
+            name: string;
+            description: string;
+            icon?: string;
+            image?: string;
+            inDevelopment: boolean;
+        }
+    ) {
+        super(id, _stages, props);
+        this.name = props.name;
+        this.description = props.description;
+        this.icon = props.icon;
+        this.image = props.image;
+        this.inDevelopment = props.inDevelopment;
+    }
+    name: string;
+    description: string;
+    icon?: string | undefined;
+    image?: string | undefined;
+    inDevelopment: boolean;
+}
