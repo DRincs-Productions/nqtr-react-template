f662a1aafd25bb23a3507bff30c471ecdb56951a
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-07-11 19:08:28 +0200

    Refactor time tracking functions to use hooks and update notification messages

--- src/hooks/useTimeTracker.ts
+++ src/hooks/useTimeTracker.ts
@@ -1,39 +1,48 @@
 import { navigator, OnRunProps, questsNotebook, routine, timeTracker } from "@drincs/nqtr";
 import { storage } from "@drincs/pixi-vn";
 import { useSnackbar } from "notistack";
+import { useCallback } from "react";
+import { useTranslation } from "react-i18next";
 
 const NOT_CAN_SPEND_TIME_FLAG_KEY = "not_can_spend_time";
 
-export function sleep(newDayHour: number, props: OnRunProps): boolean {
-    if (storage.getFlag(NOT_CAN_SPEND_TIME_FLAG_KEY)) {
-        props.notify("You can't sleep now");
-        return false;
-    }
-    timeTracker.increaseDate(newDayHour);
-    routine.clearExpiredRoutine();
-    navigator.clearExpiredActivities();
-    questsNotebook.startsStageMustBeStarted(props);
-    return true;
-}
-
-export function wait(timeSpent: number, notify: (message: string) => void): boolean {
-    if (storage.getFlag(NOT_CAN_SPEND_TIME_FLAG_KEY)) {
-        notify("You can't sleep now");
-        return false;
-    }
-    if (timeTracker.currentTime + timeSpent >= 23 || timeTracker.currentTime < 5) {
-        notify("You can't wait anymore, you should sleep now");
-        return false;
-    }
-    timeTracker.increaseTime(timeSpent);
-    return true;
-}
-
 export default function useTimeTracker() {
     const { enqueueSnackbar } = useSnackbar();
+    const { t: uiTransition } = useTranslation(["ui"]);
+
+    const sleep = useCallback(
+        (newDayHour: number, props: OnRunProps) => {
+            if (storage.getFlag(NOT_CAN_SPEND_TIME_FLAG_KEY)) {
+                props.notify(uiTransition("cant_sleep_now"));
+                return false;
+            }
+            timeTracker.increaseDate(newDayHour);
+            routine.clearExpiredRoutine();
+            navigator.clearExpiredActivities();
+            questsNotebook.startsStageMustBeStarted(props);
+            return true;
+        },
+        [enqueueSnackbar]
+    );
+
+    const wait = useCallback(
+        (timeSpent: number) => {
+            if (storage.getFlag(NOT_CAN_SPEND_TIME_FLAG_KEY)) {
+                enqueueSnackbar(uiTransition("cant_sleep_now"));
+                return false;
+            }
+            if (timeTracker.currentTime + timeSpent >= 23 || timeTracker.currentTime < 5) {
+                enqueueSnackbar(uiTransition("cant_wait_now"));
+                return false;
+            }
+            timeTracker.increaseTime(timeSpent);
+            return true;
+        },
+        [enqueueSnackbar]
+    );
 
     return {
         sleep,
-        wait: (timeSpent: number) => wait(timeSpent, enqueueSnackbar),
+        wait,
     };
 }
