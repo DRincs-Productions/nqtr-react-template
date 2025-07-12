2cf59a1b7e17015be816daae953b11d91fbe9c4e
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-07-12 12:58:11 +0200

    Refactor QuickActivities to improve layout and remove AnimatePresence for better performance

--- src/screens/nqtr/QuickActivities.tsx
+++ src/screens/nqtr/QuickActivities.tsx
@@ -1,5 +1,4 @@
 import { OnRunAsyncFunction } from "@drincs/nqtr";
-import { AnimatePresence } from "motion/react";
 import { useMemo } from "react";
 import { useShallow } from "zustand/react/shallow";
 import { NqtrRoundIconButtonConvertor } from "../../components/NqtrRoundIconButton.tsx";
@@ -29,7 +28,7 @@ export default function QuickActivities() {
             justifyContent='center'
             alignItems='flex-end'
             spacing={0.5}
-            maxLeght={"100%"}
+            maxLeght={"80%"}
             sx={{
                 display: "flex",
                 position: "absolute",
@@ -39,26 +38,24 @@ export default function QuickActivities() {
             }}
             className={hidden ? `motion-opacity-out-0 motion-translate-x-out-[50%]` : undefined}
         >
-            <AnimatePresence>
-                {activities.map((item) => (
-                    <NqtrRoundIconButtonConvertor
-                        key={"activity" + item.id}
-                        disabled={item.disabled}
-                        onClick={() => onClick(item.run)}
-                        ariaLabel={t(item.name)}
-                        image={item.icon}
-                    />
-                ))}
-                {routine.map((item) => (
-                    <NqtrRoundIconButtonConvertor
-                        key={"commitment" + item.id}
-                        disabled={item.disabled}
-                        onClick={() => onClick(item.run)}
-                        ariaLabel={t(item.name)}
-                        image={item.icon}
-                    />
-                ))}
-            </AnimatePresence>
+            {activities.map((item) => (
+                <NqtrRoundIconButtonConvertor
+                    key={"activity" + item.id}
+                    disabled={item.disabled}
+                    onClick={() => onClick(item.run)}
+                    ariaLabel={t(item.name)}
+                    image={item.icon}
+                />
+            ))}
+            {routine.map((item) => (
+                <NqtrRoundIconButtonConvertor
+                    key={"commitment" + item.id}
+                    disabled={item.disabled}
+                    onClick={() => onClick(item.run)}
+                    ariaLabel={t(item.name)}
+                    image={item.icon}
+                />
+            ))}
         </StackOverflow>
     );
 }
