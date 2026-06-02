7e9152225ff0e13127a6e773d9dc7e254b5ec5b4
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-06-02 23:50:34 +0200

    refactor: update useImageSrc to accept TimeSlotsImage type

--- src/lib/hooks/image-hooks.ts
+++ src/lib/hooks/image-hooks.ts
@@ -1,10 +1,14 @@
 import { getPixiJSAsset } from "@/lib/utils/assets-utility";
+import TimeSlotsImage from "@/models/TimeSlotsImage";
 import { useMemo } from "react";
 
-export function useImageSrc(src?: string | null): string | undefined {
+export function useImageSrc(src?: string | TimeSlotsImage | null): string | undefined {
     return useMemo(() => {
         if (!src) {
             return undefined;
+        }
+        if (src instanceof TimeSlotsImage) {
+            src = src.src;
         }
         return getPixiJSAsset(src);
     }, [src]);
