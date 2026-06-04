ea8046fbd47749df03b2e4ddd8a6d44835ee2ab8
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-06-04 18:25:22 +0200

    feat: integrate time tracking functionality into useRoomSync and remove useTimeTracker hook

--- src/lib/hooks/nqtr-hooks.ts
+++ src/lib/hooks/nqtr-hooks.ts
@@ -2,8 +2,9 @@ import { CANVAS_UI_LAYER_NAME } from "@/constants";
 import { useGameProps } from "@/lib/hooks/props-hooks";
 import { useQueryCurrentRoom } from "@/lib/query/room-query";
 import { GameStatus } from "@/lib/stores/game-status-store";
-import { canvas } from "@drincs/pixi-vn";
-import { useEffect } from "react";
+import { navigator, questsNotebook, routine, timeTracker } from "@drincs/nqtr";
+import { canvas, storage } from "@drincs/pixi-vn";
+import { useCallback, useEffect } from "react";
 
 export function useRoomSync() {
     const { data: { room: currentRoom, background, activities, routine } = {} } =
@@ -41,4 +42,45 @@ export function useRoomSync() {
     }, [currentRoom, background, activities, routine, gameProps]);
 
     return null;
+}
+
+const NOT_CAN_SPEND_TIME_FLAG_KEY = "not_can_spend_time";
+export default function useTimeTracker() {
+    const props = useGameProps();
+
+    const sleep = useCallback(
+        (newDayHour: number) => {
+            if (storage.getFlag(NOT_CAN_SPEND_TIME_FLAG_KEY)) {
+                props.toast(props.uiTransition("cant_sleep_now"));
+                return false;
+            }
+            timeTracker.increaseDate(1, newDayHour);
+            routine.clearExpiredRoutine();
+            navigator.clearExpiredActivities();
+            questsNotebook.startsStageMustBeStarted(props);
+            return true;
+        },
+        [props],
+    );
+
+    const wait = useCallback(
+        (timeSpent: number) => {
+            if (storage.getFlag(NOT_CAN_SPEND_TIME_FLAG_KEY)) {
+                props.toast(props.uiTransition("cant_sleep_now"));
+                return false;
+            }
+            if (timeTracker.currentTime + timeSpent >= 23 || timeTracker.currentTime < 5) {
+                props.toast(props.uiTransition("cant_wait_now"));
+                return false;
+            }
+            timeTracker.increaseTime(timeSpent);
+            return true;
+        },
+        [props],
+    );
+
+    return {
+        sleep,
+        wait,
+    };
 }
