6a0f64f781cd7d4041198ca536ade3ee68ebb88b
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-07-01 20:37:03 +0200

    improve

--- vite.config.ts
+++ vite.config.ts
@@ -58,7 +58,7 @@ export default defineConfig(({ mode }) => ({
             includeAssets: ["favicon.ico", "apple-touch-icon-180x180.png"],
             manifest: {
                 name: "my-app-project-name",
-                short_name: "Visual Novel Example",
+                short_name: "Point & Click Example",
                 description: "my-app-description",
                 theme_color: "#ffffff",
                 start_url: "/",
